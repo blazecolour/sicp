76df2418186113cb15c40a31db2f50de6d0183e9
blazecolour <936x936@gmail.com>
2017-12-17 23:05:19 +0300

    fix ex1_11.rkt

--- src/sicp/chapter_01/1_11.rkt
+++ src/sicp/chapter_01/1_11.rkt
@@ -6,3 +6,12 @@
   (+ (f (- n 1))
      (f (- n 2))
      (f (- n 3)))))
+
+(define (f-it n)
+  (define (f-iter a b c counter)
+    (if (< n 3)
+        n        
+        (if (> counter n)
+            a
+            (f-iter (+ a b c) a b (+ counter 1)))))
+  (f-iter 2 1 0 3))

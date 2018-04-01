7ef5aa763a31526aabc0f57589e137714e519593
blazecolour <936x936@gmail.com>
2018-04-01 22:05:08 +0300

    fix tests

--- tests/sicp/chapter_01/1_03_test.rkt
+++ tests/sicp/chapter_01/1_03_test.rkt
@@ -2,6 +2,6 @@
 
 (require rackunit "../../../src/sicp/chapter_01/1_03.rkt")
 
-(check-equal? (sum-of-squares-max 2 3 3) 18)
-(check-equal? (sum-of-squares-max 3 4 4) 32)
-(check-equal? (sum-of-squares-max 3 7 4) 65)
+(check-equal? (sum-of-squares-max 2 3 2) 13)
+(check-equal? (sum-of-squares-max 3 5 6) 61)
+(check-equal? (sum-of-squares-max 5 8 3) 89)

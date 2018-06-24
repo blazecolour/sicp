809cd2f76d6bb25d4e5d6c901587a3f5e8f2c4c0
blazecolour <936x936@gmail.com>
2018-06-24 14:30:08 +0300

    fix doc

--- tests/sicp/chapter_01/1_04_test.rkt
+++ tests/sicp/chapter_01/1_04_test.rkt
@@ -2,7 +2,7 @@
 
 (require rackunit "../../../src/sicp/chapter_01/1_04.rkt")
 
-(check-equal? (a-plus-abs-b 2 3) 5)
-(check-equal? (a-plus-abs-b 4 -3) 7)
-(check-equal? (a-plus-abs-b -3 0) -3)
-(check-equal? (a-plus-abs-b -3 4) 1)
+(check-equal? (a-plus-abs-b 3 5) 8)
+(check-equal? (a-plus-abs-b 5 -5) 10)
+(check-equal? (a-plus-abs-b -1 0) -1)
+(check-equal? (a-plus-abs-b -7 2) -5)
